d9c6661896370f68652c5c90f12eeb1de2d22bc0
Josh <89800964+JoshieFur@users.noreply.github.com>
2021-09-03 14:18:26 -0400

    New Project

---
+++ Practice 2- Calculator.cpp
@@ -0,0 +1,40 @@
+// Practice 3- Calculator
+// Josh T
+// Version 1.0.0 uploaded to github on 9-1-21
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int c;
+    float a, b, d;
+    cout << "----------------------------------------" << endl
+        << "-----Welcome to Joshie's Calculator-----" << endl
+        << "-----------------V 1.1------------------" << endl
+        << "----------------------------------------" << endl << endl;
+
+    cout << "Enter your first number.\n";
+    cin >> a;
+    cout << "Enter your second number.\n";
+    cin >> b;
+    cout << "Choose a operation. (+ is 1,- is 2,* is 3, or / is 4)\n";
+    cin >> c;
+    if (c == 1) {
+        d = a + b;
+    }
+    else if (c == 2) {
+        d = a - b;
+    }
+    else if (c == 3) {
+        d = a * b;
+    }
+    else if (c == 4) {
+        d = float(a) / float(b);
+    }
+
+    cout << d << endl;
+
+    return 0;
+}
